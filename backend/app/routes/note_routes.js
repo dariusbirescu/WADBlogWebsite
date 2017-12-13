40f0fe335c3ed48741f6bbd5b2f141dc49c0b838
Darius Birescu <darius.birescu@sap.com>
2017-12-13 18:13:50 +0200

    Initialization

--- backend/app/routes/note_routes.js
+++ backend/app/routes/note_routes.js
@@ -1,54 +1,54 @@
 // note_routes.js
 module.exports = function(app, db) {
-  app.post('/notes', (req, res) => {
-    const note = { text: req.body.body, title: req.body.title };
-    db.collection('notes').insert(note, (err, result) => {
-      if (err) { 
-        res.send({ 'error': 'An error has occurred' }); 
-      } else {
-        res.send(result);
-      }
-    });
-  });
+  // app.post('/notes', (req, res) => {
+  //   const note = { text: req.body.body, title: req.body.title };
+  //   db.collection('notes').insert(note, (err, result) => {
+  //     if (err) { 
+  //       res.send({ 'error': 'An error has occurred' }); 
+  //     } else {
+  //       res.send(result);
+  //     }
+  //   });
+  // });
 
-  var ObjectID = require('mongodb').ObjectID;  
+   var ObjectID = require('mongodb').ObjectID;  
   
-  app.get('/notes/:id', (req, res) => {
-    const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
-    db.collection('notes').findOne(details, (err, item) => {
-      if (err) {
-        res.send({'error':'An error has occurred'});
-      } else {
-        res.send(item);
-      }
-    });
-  });
+  // app.get('/notes/:id', (req, res) => {
+  //   const id = req.params.id;
+  //   const details = { '_id': new ObjectID(id) };
+  //   db.collection('notes').findOne(details, (err, item) => {
+  //     if (err) {
+  //       res.send({'error':'An error has occurred'});
+  //     } else {
+  //       res.send(item);
+  //     }
+  //   });
+  // });
 
-  app.delete('/notes/:id', (req, res) => {
-    const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
-    db.collection('notes').remove(details, (err, item) => {
-      if (err) {
-        res.send({'error':'An error has occurred'});
-      } else {
-        res.send('Note ' + id + ' deleted!');
-      } 
-    });
-  });
+  // app.delete('/notes/:id', (req, res) => {
+  //   const id = req.params.id;
+  //   const details = { '_id': new ObjectID(id) };
+  //   db.collection('notes').remove(details, (err, item) => {
+  //     if (err) {
+  //       res.send({'error':'An error has occurred'});
+  //     } else {
+  //       res.send('Note ' + id + ' deleted!');
+  //     } 
+  //   });
+  // });
 
-  app.put('/notes/:id', (req, res) => {
-    const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
-    const note = { text: req.body.body, title: req.body.title };
-    db.collection('notes').update(details, note, (err, result) => {
-      if (err) {
-          res.send({'error':'An error has occurred'});
-      } else {
-          res.send(note);
-      } 
-    });
-  });
+  // app.put('/notes/:id', (req, res) => {
+  //   const id = req.params.id;
+  //   const details = { '_id': new ObjectID(id) };
+  //   const note = { text: req.body.body, title: req.body.title };
+  //   db.collection('notes').update(details, note, (err, result) => {
+  //     if (err) {
+  //         res.send({'error':'An error has occurred'});
+  //     } else {
+  //         res.send(note);
+  //     } 
+  //   });
+  // });
 
   app.post('/users', (req, res) => {
     const note = { name: req.body.name, password: req.body.password };
@@ -61,15 +61,35 @@ module.exports = function(app, db) {
     });
   });
 
-  app.post('/articles', (req, res) => {
-    const article = { title: req.body.title, content: req.body.content, userId: req.body.userId };
-    db.collection('articles').insert(article, (err, result) => {
-      if (err) { 
-        res.send({ 'error': 'An error has occurred' }); 
+  app.get('/users', (req, res) => {
+    db.collection("users").find({}).toArray(function(err, result) {
+          if (err) throw err;
+          console.log(result);
+          db.close();
+        }); 
+    });
+
+     app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('users').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
       } else {
-        res.send(result);
+        res.send(item);
       }
     });
   });
+
+  // app.post('/articles', (req, res) => {
+  //   const article = { title: req.body.title, content: req.body.content, userId: req.body.userId };
+  //   db.collection('articles').insert(article, (err, result) => {
+  //     if (err) { 
+  //       res.send({ 'error': 'An error has occurred' }); 
+  //     } else {
+  //       res.send(result);
+  //     }
+  //   });
+  // });
 
 };
